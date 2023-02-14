f19d7c44880471f04035cf385fbff0df164d732d
Николай Алешинков <aleshinkov91@gmail.com>
2023-02-15 00:52:58 +0300

    add models

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Comment(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comment'
+    )
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.text
+
+
+class Review(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments'
+    )
+    score = models.PositiveIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(10)]
+    )
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    comment = models.ForeignKey(
+        Comment, on_delete=models.SET_NULL, related_name='comments'
+    )
+
+    def __str__(self) -> str:
+        return self.text
