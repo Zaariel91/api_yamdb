a4a7cb35643b145c9351fb0b3505ac43cc0af0e2
Nikolay <aleshinkov91@gmail.com>
2023-02-15 15:28:53 +0300

    add template for view and serializers

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from titles.models import Comment, Review
+from .serializers import CommentSerializer, ReviewSerializer
 
-# Create your views here.
+
+class APICommentViewset(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+class APIReviewViewset(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
 
 
 class Comment(models.Model):
